40bdc6cdcc7e3f3a19e9cb199e188af520c1d660
Wesley Rodrigues dos Reis <wesleyepolly16@gmail.com>
2022-01-31 05:55:41 -0300

    busca com input

--- src/components/table/TableBase.vue
+++ src/components/table/TableBase.vue
@@ -17,18 +17,12 @@
         >
           <div class="flex justify-between items-center">
             <span>{{ th.text }}</span>
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
-                />
+            <span class="cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 -mb-2" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
               </svg>
             </span>
           </div>
@@ -42,14 +36,14 @@
         :key="td"
         class="border-b odd:bg-white even:bg-gray-50 text-left"
       >
-        <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap">
+        <td v-if="!isColId" class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap">
           {{ td.id }}
         </td>
         <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap">
-          {{ td.name }}
+          <router-link class="text-sky-700" :to="{name:'Home'}">{{ td.name }}</router-link>
         </td>
         <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap">
-          {{ td.office }}
+           <router-link class="text-sky-700" :to="{name:'Home'}">{{ td.office }}</router-link>
         </td>
         <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap">
           {{ td.startData }}
@@ -68,6 +62,7 @@ export default {
   props: {
     columns: Array,
     entries: Array,
+    isColId:Boolean,
   },
   computed: {
     tableHeader() {
--- src/views/DataTable.vue
+++ src/views/DataTable.vue
@@ -1,100 +1,121 @@
 <template>
+  <div>
     <h1 class="text-center text-3xl text-gray-500">Data Table</h1>
 
-  <div class="container bg-white mx-auto p-5">
-    <div class="m-3 overflow-hidden overflow-x-auto">
-      <div
-        class="
-          flex flex-col
-          items-start
-          gap-1
-          md:flex-row md:justify-between md:items-center
-          mb-2
-        "
-      >
-        <!-- Number item List -->
-        <div class="flex justify-start items-center gap-2">
-          <select
-          v-model="currentEntries"
-          @change="paginateEntries"
-            name="quantItem"
-            id="quantItem"
-            class="
-              text-sm text-gray-600
-              p-[0.18rem]
-              rounded-sm
-              border
-              outline-none
-              focus:border-sky-700
-            "
-          >
-            <option v-for="se in showEntries" :key="se" :value="se">{{ se }}</option>
-          </select>
-          <label for="quantItem" class="text-sm text-gray-500"
-            >resutados por página</label
-          >
+    <div class="container bg-white mx-auto p-5">
+      <div class="m-3 overflow-hidden overflow-x-auto">
+        <div
+          class="
+            flex flex-col
+            items-start
+            gap-1
+            md:flex-row md:justify-between md:items-center
+            mb-2
+          "
+        >
+          <!-- Number item List -->
+          <div class="flex justify-start items-center gap-2">
+            <select
+              v-model="currentEntries"
+              @change="paginateEntries"
+              name="quantItem"
+              id="quantItem"
+              class="
+                text-sm text-gray-600
+                p-1
+                rounded-sm
+                border
+                outline-none
+                focus:border-sky-700
+                bg-transparent
+              "
+            >
+              <option v-for="se in showEntries" :key="se" :value="se">
+                {{ se }}
+              </option>
+            </select>
+            <label for="quantItem" class="text-sm text-gray-500"
+              >resutados por página</label
+            >
+          </div>
+          <!-- Search -->
+          <div class="flex justify-start items-center gap-2">
+            <label class="text-sm text-gray-500" for="filter-table"
+              >Pesquisar</label
+            >
+            <input
+              v-model="searchInput"
+              @keyup="searchEvent"
+              class="
+                text-sm text-gray-600
+                p-1
+                rounded-sm
+                border
+                outline-none
+                focus:border-sky-700
+              "
+              type="text"
+            />
+          </div>
         </div>
-        <!-- Search -->
-        <div class="flex justify-start items-center gap-2">
-          <label class="text-sm text-gray-500" for="filter-table"
-            >Pesquisar</label
-          >
-          <input
-            class="
-              text-sm text-gray-600
-              p-1
-              rounded-sm
-              border
-              outline-none
-              focus:border-sky-700
-            "
-            type="text"
-            name="filter"
-            id="filter-table"
-          />
+
+        <TableBase :columns="columns" :entries="filteredEntries" />
+
+        <div class="flex justify-between items-center mt-1">
+          <p class="text-sm text-gray-600">
+            Item {{ showInfo.from }} a {{ showInfo.to }} de
+            {{ showInfo.of }} registros
+          </p>
+
+          <ul class="flex justify-start items-center gap-[2px]">
+            <li
+               class="py-1 overflow-hidden rounded-sm bg-white border border-sky-800"
+             :class="['bg-sky-700 text-white', { 'opacity-50 cursor-not-allowed': currentPage === 1 }]">
+              <a class="px-2 py-1 flex justify-start items-center" href="#" @click.prevent="(currentPage = 1), paginateEntries()"
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+                </svg>
+                </a
+              >
+            </li>
+
+            <li
+              v-for="pag in showPagination"
+              :key="pag"
+              class="py-1 overflow-hidden rounded-sm bg-white border border-sky-800"
+              :class="[
+                'text-sky-800',
+                { ellipsis: pag === '...', 'bg-sky-700 text-gray-50': pag === currentPage },
+              ]"
+            >
+              <a class="px-3 py=2" href="#" @click.prevent="paginateEvent(pag)">{{ pag }}</a>
+            </li>
+
+            <li
+              class="py-1 overflow-hidden rounded-sm bg-white border border-sky-800"
+             :class="['bg-sky-700 text-white', {'opacity-50 cursor-not-allowed': currentPage === allPages }]">
+              <a class="px-2 py-1 flex justify-start items-center"
+                href="#"
+                @click.prevent="(currentPage = allPages), paginateEntries()"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+                  </svg>
+                </a
+              >
+            </li>
+          </ul>
+
         </div>
-      </div>
-      <TableBase :columns="columns" :entries="filteredEntries" />
-
-      <div class="flex justify-between items-center">
-        <p>item {{showInfo.from}} a {{showInfo.to}} de {{showInfo.of}} registros</p>
-        <ul class="flex justify-start items-center gap-2">
-          <li :class="['nav-item', {'disabled':currentPage === 1 }]">
-            <a href="#" @click.prevent="currentPage = 1, paginateEntries()">First</a>
-          </li>
-
-          <li :class="['nav-item', {'disabled':currentPage === 1 }]">
-            <a href="#" @click.prevent="(currentPage < 1) ? currentPage = 1 : currentPage -= 1
-            , paginateEntries()">Prev</a>
-          </li>
-
-          <li v-for="pag in showPagination" :key="pag" :class="['nav-item', {'ellipsis':pag === '...', 'active':pag === currentPage}]">
-            <a href="#" @click.prevent="paginateEvent(pag)">{{pag}}</a>
-          </li>
-
-          <li :class="['nav-item', {'disabled':currentPage === allPages }]">
-            <a href="#" @click.prevent="(currentPage > allPages) ? currentPage = allPages : currentPage += 1
-              , paginateEntries()">Next</a>
-          </li>
-
-          <li :class="['nav-item', {'disabled':currentPage === allPages }]">
-            <a href="#" @click.prevent="currentPage = allPages, paginateEntries()">Last</a>
-          </li>
-
-        </ul>
       </div>
     </div>
   </div>
-
-  <p>
-    https://www.youtube.com/watch?v=OgutXmQDq-k&t=174s
-  </p>
 </template>
 
 <script>
-import * as $ from 'alga-js'
+import * as $ from "alga-js";
 import TableBase from "../components/table/TableBase.vue";
-
 
 export default {
   name: "DataTable",
@@ -745,33 +766,54 @@ export default {
       showEntries: [5, 10, 15, 25, 50, 75, 100],
       currentEntries: 5,
       filteredEntries: [],
-      currentPage:1,
-      allPages:1
+      currentPage: 1,
+      allPages: 1,
+      searchInput: "",
+      searchEntries: [],
+     
     };
   },
-  computed:{
-    showInfo(){
-      return $.array.show(this.entries, this.currentPage, this.currentEntries)
+
+  computed: {
+    showInfo() {
+      const getCurrentEntries = this.searchEntries.length <= 0 ? this.entries : this.searchEntries;
+      return $.array.show(getCurrentEntries, this.currentPage, this.currentEntries);
     },
 
-    showPagination(){
-      return $.array.pagination(this.allPages, this.currentPage, 2)
-    }
+    showPagination() {
+      return $.array.pagination(this.allPages, this.currentPage, 2);
+    },
   },
   created() {
-      this.filteredEntries = $.array.paginate(this.entries, this.currentPage, this.currentEntries)
-      this.allPages = $.array.pages(this.entries, this.currentEntries)
+    this.filteredEntries = $.array.paginate(
+      this.entries,
+      this.currentPage,
+      this.currentEntries
+    );
+    this.allPages = $.array.pages(this.entries, this.currentEntries);
   },
   methods: {
-    paginateEntries(){
-      this.filteredEntries = $.array.paginate(this.entries, this.currentPage, this.currentEntries)
+    paginateEntries() {
+      if (this.searchInput.length >= 3) {
+        this.searchEntries = $.array.search(this.entries, this.searchInput);
+        this.filteredEntries = $.array.paginate(this.searchEntries, this.currentPage, this.currentEntries);
+        this.allPages = $.array.pages(this.searchEntries, this.currentEntries);
+      } else {
+        this.searchEntries = [];
+        this.filteredEntries = $.array.paginate(this.entries, this.currentPage, this.currentEntries);
+        this.allPages = $.array.pages(this.entries, this.currentEntries);
+      }
     },
 
-    paginateEvent(page){
-      this.currentPage = page
-      this.paginateEntries()
-    }
+    paginateEvent(page) {
+      this.currentPage = page;
+      this.paginateEntries();
+    },
 
+    searchEvent() {
+      this.currentPage = 1;
+      this.paginateEntries();
+    },
   },
 };
 </script>
